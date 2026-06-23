0ee796fafd77c0553679f62f94592fd8e03c913d
cristianxcueva <crizzydablack@gmail.com>
2026-06-23 14:09:53 -0400

    Initial commit - backend (Lambda, tests, Terraform with S3 remote state)

---
+++ terraform/main.tf
@@ -0,0 +1,270 @@
+terraform {
+  backend "s3" {
+    bucket = "cristianxcueva-terraform-state"
+    key    = "cloud-resume-challenge/terraform.tfstate"
+    region = "us-east-1"
+  }
+ required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+#s3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+
+  bucket = "cristianxcueva.dev"
+}       
+
+#s3 bucket public access block
+resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
+
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}       
+
+#aws s3 bucket website configuration
+resource "aws_s3_bucket_website_configuration" "my_bucket_website" {
+  bucket = aws_s3_bucket.my_bucket.id   
+    index_document {
+        suffix = "index.html"
+    }   
+
+    error_document {
+        key = "error.html"
+    }
+}
+
+#aws s3 bucket policy
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+    depends_on = [aws_s3_bucket_public_access_block.my_bucket_public_access_block]
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+        {
+            Effect = "Allow"
+            Principal = "*"
+            Action = "s3:GetObject"
+            Resource = "${aws_s3_bucket.my_bucket.arn}/*"
+        }
+        ]
+    })
+}
+
+#aws acm certificate
+resource "aws_acm_certificate" "my_certificate" {
+  domain_name       = "cristianxcueva.dev"
+  validation_method = "DNS"
+}
+#aws route53 zone
+resource "aws_route53_zone" "main" {
+  name = "cristianxcueva.dev"
+}
+
+#aws route53 record
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.my_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+  allow_overwrite = true
+  zone_id = aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+#aws acm certificate validation
+resource "aws_acm_certificate_validation" "my_certificate_validation" {
+  certificate_arn         = aws_acm_certificate.my_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+#cloudfront distribution
+resource "aws_cloudfront_distribution" "my_distribution" {
+  aliases             = ["cristianxcueva.dev"]
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.my_bucket_website.website_endpoint
+    origin_id   = "s3-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate_validation.my_certificate_validation.certificate_arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+}
+
+#aws route53 record for cloudfront distribution alias
+resource "aws_route53_record" "cloudfront_alias" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "cristianxcueva.dev"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.my_distribution.domain_name
+    zone_id                = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
+
+#dynamodb table for visitor count
+resource "aws_dynamodb_table" "visitor_count_table" {
+  name         = "visitor_count"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+#iam role for lambda function
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]   
+  })
+}
+
+#dynamodb policy for lambda function
+resource "aws_iam_role_policy" "dynamodb_policy" {
+  name = "dynamodb_policy"
+  role = aws_iam_role.lambda_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem"
+        ]
+        Effect   = "Allow"
+        Resource = aws_dynamodb_table.visitor_count_table.arn
+      }
+    ]
+  })
+}
+
+#aws cloudwatch role policy for lambda function
+resource "aws_iam_role_policy_attachment" "lambda_logging" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+#lambda function for visitor count zip
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file  = "${path.module}/../lambda/lambda_function.py"
+  output_path = "${path.module}/../lambda/lambda_function.zip"
+}
+
+#aws lambda function for visitor count
+resource "aws_lambda_function" "visitor_count_lambda" {
+  function_name = "visitor_count_lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+} 
+
+#aws api gateway http api for visitor count
+resource "aws_apigatewayv2_api" "visitor_count_api" {
+  name          = "visitor_count_api"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = ["https://cristianxcueva.dev"]
+    allow_methods = ["GET"]
+    allow_headers = ["content-type"]
+  }
+}
+
+#aws api gateway integration for visitor count
+resource "aws_apigatewayv2_integration" "visitor_count_integration" {
+  api_id           = aws_apigatewayv2_api.visitor_count_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.visitor_count_lambda.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+} 
+
+#aws api gateway route for visitor count
+resource "aws_apigatewayv2_route" "visitor_count_route" {
+  api_id    = aws_apigatewayv2_api.visitor_count_api.id
+  route_key = "GET /visitor-count"
+target    = "integrations/${aws_apigatewayv2_integration.visitor_count_integration.id}"
+} 
+
+#aws api gateway stage for visitor count
+resource "aws_apigatewayv2_stage" "visitor_count_stage" {
+  api_id      = aws_apigatewayv2_api.visitor_count_api.id
+  name        = "$default"
+  auto_deploy = true
+} 
+
+#aws lambda permission for api gateway
+resource "aws_lambda_permission" "api_gateway_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visitor_count_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.visitor_count_api.execution_arn}/*/*"
+}
